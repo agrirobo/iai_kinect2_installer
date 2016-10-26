813ad503be182611236cd1d30e2da497ec4b4b31
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-26 14:22:18 +0900

    Add iai_kinect2 building
    TODO: installation

--- auto_install.sh
+++ auto_install.sh
@@ -10,11 +10,25 @@ echo "The section of libfreenect2 installation"
 cd libfreenect2
 echo "Requirements installation (on libfreenect2)"
 sudo apt-get install libusb-1.0-0-dev libturbojpeg libjpeg-turbo8-dev libglfw3-dev beignet-dev libva-dev libopenni2-dev
+sudo apt-get install beignet opencl-headers
 echo "libfreenect2 installation"
 mkdir build && cd build
 cmake .. -DENABLE_CXX11=ON
 make
 sudo make install
 sudo cp ../platform/linux/udev/90-kinect2.rules /etc/udev/rules.d/
+cd $topDir
 echo "finish installation of libfreenect2"
+echo "The section of iai_kinect2 installation"
+echo "Requirements installation (on iai_kinect2)"
+cd src/iai_kinect2
+rosdep install -r --from-paths .
+cd $topDir
+echo "catkin_workspace constructing"
+cd src
+catkin_init_workspace
+cd $topDir
+echo "iai_kinect2 installation"
+catkin_make -DCMAKE_BUILD_TYPE="Release"
+. devel/setup.bash
 cd $topDir
